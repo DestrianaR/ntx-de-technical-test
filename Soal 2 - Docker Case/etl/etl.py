c8bfb1fdf74edbcad524b6b32f9da6f774752cee
DestrianaR <destrianaramadani2@gmail.com>
2025-04-26 19:13:48 +0700

    Update etl script, Dockerfile and requirements.txt

--- Soal 2 - Docker Case/etl/etl.py
+++ Soal 2 - Docker Case/etl/etl.py
@@ -1,6 +1,7 @@
-import asyncio
-import httpx
+import asyncio      # Import asyncio to run asynchronous code
+import httpx        # Import httpx, an async HTTP client
 
+# Sentences to send to the API
 sentences = [
     "Saya suka makan nasi goreng.",
     "Hari ini cuaca sangat panas.",
@@ -14,22 +15,21 @@ sentences = [
     "Kami berencana untuk berlibur ke Bali.",
 ]
 
+# The URL of the API endpoint to which the sentences will be sent
 api_url = "http://api:6000/predict"
 
-
+# Send each sentence to the API and print the response
 async def main():
     for sentence in sentences:
         try:
             async with httpx.AsyncClient() as aclient:
                 response = await aclient.post(api_url, params={"text": sentence})
-
                 response.raise_for_status()
-
                 print(response.json())
         except Exception as e:
             print(e)
             continue
 
-
+# Run the async main function
 if __name__ == "__main__":
     asyncio.run(main())
